174a00b196bee686018a0d154349515f05f74529
mack <mack@itu.dk>
2021-11-24 21:55:00 +0100

    Random meal generator source

---
+++ RandomMealGenerator/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RandomMealGenerator.Models;
+
+namespace RandomMealGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        static WebClient client = new WebClient();
+        public JObject GetProducts()
+        {
+
+            var json = client.DownloadString("https://www.themealdb.com/api/json/v1/1/random.php");
+            dynamic ans = JObject.Parse(json);
+            return ans;
+
+        }
+
+        public IActionResult Index()
+        {
+            var products = GetProducts();
+            ViewBag.products = products;
+
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
---
+++ RandomMealGenerator/Models/ProductModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RandomMealGenerator.Models
+{
+    public class ProductModel
+    {
+ 
+        public class Rootobject
+        {
+            public Meal[] meals { get; set; }
+        }
+
+        public class Meal
+        {
+            public string strMeal { get; set; }
+            public string strCategory { get; set; }
+            public string strArea { get; set; }
+            public string strInstructions { get; set; }
+            public string strMealThumb { get; set; }
+            public string strIngredient1 { get; set; }
+            public string strIngredient2 { get; set; }
+            public string strIngredient3 { get; set; }
+            public string strIngredient4 { get; set; }
+            public string strIngredient5 { get; set; }
+            public string strIngredient6 { get; set; }
+            public string strIngredient7 { get; set; }
+            public string strIngredient8 { get; set; }
+            public string strIngredient9 { get; set; }
+            public string strIngredient10 { get; set; }
+            public string strIngredient11 { get; set; }
+            public string strIngredient12 { get; set; }
+            public string strIngredient13 { get; set; }
+            public string strIngredient14 { get; set; }
+            public string strIngredient15 { get; set; }
+            public string strIngredient16 { get; set; }
+            public string strIngredient17 { get; set; }
+            public string strIngredient18 { get; set; }
+            public string strIngredient19 { get; set; }
+            public string strIngredient20 { get; set; }
+
+        }
+
+    }
+}
